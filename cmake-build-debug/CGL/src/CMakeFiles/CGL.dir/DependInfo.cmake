522dbeb4c3a1e416325dc13d5aae33d3eb3892a6
Nguyen Văn Phi <5081237-gungui@users.noreply.gitlab.com>
2021-09-12 08:32:08 +0700

    initialized

---
+++ cmake-build-debug/CGL/src/CMakeFiles/CGL.dir/DependInfo.cmake
@@ -0,0 +1,45 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/admin/CLionProjects/hw0-intro/CGL/src/base64.cpp" "C:/Users/admin/CLionProjects/hw0-intro/cmake-build-debug/CGL/src/CMakeFiles/CGL.dir/base64.cpp.obj"
+  "C:/Users/admin/CLionProjects/hw0-intro/CGL/src/color.cpp" "C:/Users/admin/CLionProjects/hw0-intro/cmake-build-debug/CGL/src/CMakeFiles/CGL.dir/color.cpp.obj"
+  "C:/Users/admin/CLionProjects/hw0-intro/CGL/src/complex.cpp" "C:/Users/admin/CLionProjects/hw0-intro/cmake-build-debug/CGL/src/CMakeFiles/CGL.dir/complex.cpp.obj"
+  "C:/Users/admin/CLionProjects/hw0-intro/CGL/src/lodepng.cpp" "C:/Users/admin/CLionProjects/hw0-intro/cmake-build-debug/CGL/src/CMakeFiles/CGL.dir/lodepng.cpp.obj"
+  "C:/Users/admin/CLionProjects/hw0-intro/CGL/src/matrix3x3.cpp" "C:/Users/admin/CLionProjects/hw0-intro/cmake-build-debug/CGL/src/CMakeFiles/CGL.dir/matrix3x3.cpp.obj"
+  "C:/Users/admin/CLionProjects/hw0-intro/CGL/src/matrix4x4.cpp" "C:/Users/admin/CLionProjects/hw0-intro/cmake-build-debug/CGL/src/CMakeFiles/CGL.dir/matrix4x4.cpp.obj"
+  "C:/Users/admin/CLionProjects/hw0-intro/CGL/src/osdfont.cpp" "C:/Users/admin/CLionProjects/hw0-intro/cmake-build-debug/CGL/src/CMakeFiles/CGL.dir/osdfont.cpp.obj"
+  "C:/Users/admin/CLionProjects/hw0-intro/CGL/src/osdtext.cpp" "C:/Users/admin/CLionProjects/hw0-intro/cmake-build-debug/CGL/src/CMakeFiles/CGL.dir/osdtext.cpp.obj"
+  "C:/Users/admin/CLionProjects/hw0-intro/CGL/src/path.cpp" "C:/Users/admin/CLionProjects/hw0-intro/cmake-build-debug/CGL/src/CMakeFiles/CGL.dir/path.cpp.obj"
+  "C:/Users/admin/CLionProjects/hw0-intro/CGL/src/quaternion.cpp" "C:/Users/admin/CLionProjects/hw0-intro/cmake-build-debug/CGL/src/CMakeFiles/CGL.dir/quaternion.cpp.obj"
+  "C:/Users/admin/CLionProjects/hw0-intro/CGL/src/spectrum.cpp" "C:/Users/admin/CLionProjects/hw0-intro/cmake-build-debug/CGL/src/CMakeFiles/CGL.dir/spectrum.cpp.obj"
+  "C:/Users/admin/CLionProjects/hw0-intro/CGL/src/tinyxml2.cpp" "C:/Users/admin/CLionProjects/hw0-intro/cmake-build-debug/CGL/src/CMakeFiles/CGL.dir/tinyxml2.cpp.obj"
+  "C:/Users/admin/CLionProjects/hw0-intro/CGL/src/vector2D.cpp" "C:/Users/admin/CLionProjects/hw0-intro/cmake-build-debug/CGL/src/CMakeFiles/CGL.dir/vector2D.cpp.obj"
+  "C:/Users/admin/CLionProjects/hw0-intro/CGL/src/vector3D.cpp" "C:/Users/admin/CLionProjects/hw0-intro/cmake-build-debug/CGL/src/CMakeFiles/CGL.dir/vector3D.cpp.obj"
+  "C:/Users/admin/CLionProjects/hw0-intro/CGL/src/vector4D.cpp" "C:/Users/admin/CLionProjects/hw0-intro/cmake-build-debug/CGL/src/CMakeFiles/CGL.dir/vector4D.cpp.obj"
+  "C:/Users/admin/CLionProjects/hw0-intro/CGL/src/viewer.cpp" "C:/Users/admin/CLionProjects/hw0-intro/cmake-build-debug/CGL/src/CMakeFiles/CGL.dir/viewer.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "MSVC")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "GLEW_STATIC"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../CGL/include"
+  "../CGL/deps/glew/include"
+  "C:/Users/admin/vcpkg/installed/x86-windows/include"
+  "../CGL/include/CGL"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "C:/Users/admin/CLionProjects/hw0-intro/cmake-build-debug/CGL/deps/glew/CMakeFiles/glew.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/quad_test.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/admin/CLionProjects/hw0-intro/quad_test.cpp" "C:/Users/admin/CLionProjects/hw0-intro/cmake-build-debug/CMakeFiles/quad_test.dir/quad_test.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "MSVC")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../CGL/include"
+  "C:/Users/admin/vcpkg/installed/x86-windows/include"
+  "../dirent"
+  "../CGL/deps/glfw/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "C:/Users/admin/CLionProjects/hw0-intro/cmake-build-debug/CGL/src/CMakeFiles/CGL.dir/DependInfo.cmake"
+  "C:/Users/admin/CLionProjects/hw0-intro/cmake-build-debug/CGL/deps/glew/CMakeFiles/glew.dir/DependInfo.cmake"
+  "C:/Users/admin/CLionProjects/hw0-intro/cmake-build-debug/CGL/deps/glfw/src/CMakeFiles/glfw.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
